337a93a206e6b3363744fc99d49bb1bfff2806d1
yohanesokta <yohanesoktayt72@gmail.com>
2025-10-14 10:02:27 +0700

    day1: configuring g_szClass in Windows.h

--- main.c
+++ main.c
@@ -1,7 +1,57 @@
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+
+static LPSTR g_szClassName = "PQWindowClass";
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
-    return MessageBox(NULL, "hello, world", "caption", MB_OK | MB_ICONINFORMATION);
+
+    WNDCLASSEX wc = {0};
+ 
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInst;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = g_szClassName;
+    wc.lpszMenuName = NULL;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_OVERLAPPEDWINDOW,
+        g_szClassName,
+        "PQVIEW",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL, NULL, hInst, NULL);
+
+    if (hwnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
